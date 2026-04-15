cca63c30461641a07552e9427c415bad27cfd9fa
Eljena <104090886+Eljena@users.noreply.github.com>
2026-04-15 13:35:44 +0200

    add header banner

--- resources/js/components/shop/header.tsx
+++ resources/js/components/shop/header.tsx
@@ -1,14 +1,54 @@
-import { Link } from '@inertiajs/react';
+import { Link, usePage } from '@inertiajs/react';
+import { ArrowRightCircle, Search, ShoppingCart, User } from 'lucide-react';
 import logo from '@/assets/images/ates-store-logo.png';
+import { dashboard, login } from '@/routes';
 import { Button } from '../ui/button';
 import { Field } from '../ui/field';
 import { Input } from '../ui/input';
 import LinkAnimated from '../ui/link-animated';
-import { Search } from 'lucide-react';
+import { Separator } from '../ui/separator';
 
 export default function Header() {
+    const { auth } = usePage().props;
+
     return (
         <header>
+            <div className="bg-accent">
+                <div className="mx-auto flex max-w-7xl justify-end py-2">
+                    {auth.user ? (
+                        <Button asChild>
+                            <Link
+                                href={dashboard()}
+                                className="flex items-center gap-2"
+                            >
+                                Zum Admin-Bereich
+                                <ArrowRightCircle className="size-4" />
+                            </Link>
+                        </Button>
+                    ) : (
+                        <div className="flex h-7 items-center gap-2">
+                            <Button variant="link" asChild>
+                                <Link href="#">
+                                    <ShoppingCart />
+                                    Warenkorb
+                                </Link>
+                            </Button>
+
+                            <Separator
+                                orientation="vertical"
+                                className="size-0 bg-gray-500"
+                            />
+
+                            <Button asChild variant="link">
+                                <Link href={login()}>
+                                    <User className="size-4" />
+                                    Anmelden
+                                </Link>
+                            </Button>
+                        </div>
+                    )}
+                </div>
+            </div>
             <div className="mx-auto flex max-w-7xl items-center justify-between py-4">
                 <Link href="/" className="w-25">
                     <img src={logo} />
