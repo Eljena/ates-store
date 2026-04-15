7ba3368fed13dd225545f04104fef35a5f2d12a8
Eljena <104090886+Eljena@users.noreply.github.com>
2026-04-15 15:54:56 +0200

    add hero image

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -14,9 +14,19 @@ export default function Home() {
         <Layout>
             <section>
                 <div>
-                    <h1 className="py-4 text-center text-3xl font-bold">
-                        Willkommen bei Ate's store
-                    </h1>
+                    <div className="relative">
+                        <img
+                            className="h-100 w-7xl rounded-md object-cover"
+                            src="../images/philippines-landingpage.jpg"
+                        />
+                        <div className="absolute inset-0 bg-black/40" />
+                        <div className="absolute right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2">
+                            <h1 className="py-4 text-center text-3xl font-bold text-white">
+                                Willkommen bei Ate's store
+                            </h1>
+                        </div>
+                    </div>
+
                     <HomeSection title="Kategorien">
                         <div className="grid grid-cols-3 gap-5">
                             <div className="bg-amber-300 px-20 py-10">Reis</div>
