939fcfb4229ac824c9939faa2abdbbda3804a9ae
Eljena <104090886+Eljena@users.noreply.github.com>
2026-04-15 15:24:34 +0200

    replace footer links with datenschutz, impressum and agb

--- resources/js/components/shop/footer.tsx
+++ resources/js/components/shop/footer.tsx
@@ -1,13 +1,17 @@
 import LinkAnimated from '../ui/link-animated';
+import { Separator } from '../ui/separator';
 
 export default function Footer() {
     return (
         <footer>
             <div className="flex flex-col items-center gap-5 bg-accent py-10">
-                <div className="flex gap-5">
-                    <LinkAnimated href="/">Startseite</LinkAnimated>
-                    <LinkAnimated href="/products">Produkte</LinkAnimated>
-                    <LinkAnimated href="/cart">Warenkorb</LinkAnimated>
+                <div className="flex h-4 items-center gap-5">
+                    <LinkAnimated href="#">Impressum</LinkAnimated>
+                    <Separator orientation="vertical" className="bg-black" />
+                    <LinkAnimated href="#">Datenschutz</LinkAnimated>
+                    <Separator orientation="vertical" className="bg-black" />
+
+                    <LinkAnimated href="#">AGB</LinkAnimated>
                 </div>
                 <p className="text-base font-bold">&copy; 2026 Ate's store</p>
             </div>
