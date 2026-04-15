36f86a5bbfc7c530d2d503a6b3d09a56ba3d3813
Eljena <104090886+Eljena@users.noreply.github.com>
2026-04-15 13:39:18 +0200

    set max-width for content in main section

--- resources/js/layouts/shop/layout.tsx
+++ resources/js/layouts/shop/layout.tsx
@@ -10,7 +10,9 @@ export default function Layout({ children }: Props) {
     return (
         <div className="flex min-h-screen flex-col bg-white">
             <Header />
-            <main className="flex-1">{children}</main>
+            <main className="flex-1">
+                <div className="mx-auto max-w-7xl px-4 py-6">{children}</div>
+            </main>
             <Footer />
         </div>
     );
