212b7c25f078866d318b7ff2e23543d038bcf909
Eljena <104090886+Eljena@users.noreply.github.com>
2026-04-15 12:48:55 +0200

    add searchfield to navbar

--- resources/js/components/shop/header.tsx
+++ resources/js/components/shop/header.tsx
@@ -1,19 +1,33 @@
-import { Link } from "@inertiajs/react";
-import LinkAnimated from "../ui/link-animated";
+import { Link } from '@inertiajs/react';
 import logo from '@/assets/images/ates-store-logo.png';
+import { Button } from '../ui/button';
+import { Field } from '../ui/field';
+import { Input } from '../ui/input';
+import LinkAnimated from '../ui/link-animated';
+import { Search } from 'lucide-react';
 
 export default function Header() {
     return (
         <header>
-            <div className="flex max-w-7xl justify-between items-center mx-auto py-4">
+            <div className="mx-auto flex max-w-7xl items-center justify-between py-4">
                 <Link href="/" className="w-25">
-                    <img src={logo}/>
+                    <img src={logo} />
                 </Link>
 
-                <nav className="flex gap-5 text-lg">
+                <nav className="flex items-center gap-5">
                     <LinkAnimated href="/">Startseite</LinkAnimated>
                     <LinkAnimated href="/products">Produkte</LinkAnimated>
                     <LinkAnimated href="/cart">Warenkorb</LinkAnimated>
+                    <Field orientation="horizontal" className="gap-0">
+                        <Input
+                            className="rounded-full rounded-r-none"
+                            type="search"
+                            placeholder="Search..."
+                        />
+                        <Button className="rounded-full rounded-l-none">
+                            <Search />
+                        </Button>
+                    </Field>
                 </nav>
             </div>
         </header>
--- resources/js/layouts/shop/layout.tsx
+++ resources/js/layouts/shop/layout.tsx
@@ -1,19 +1,17 @@
-import { ReactNode } from "react";
-import Header from '@/components/shop/header';
+import type { ReactNode } from 'react';
 import Footer from '@/components/shop/footer';
+import Header from '@/components/shop/header';
 
 type Props = {
     children: ReactNode;
 };
 
-export default function Layout({children}: Props) {
+export default function Layout({ children }: Props) {
     return (
-        <div className="min-h-screen flex flex-col bg-white">
+        <div className="flex min-h-screen flex-col bg-white">
             <Header />
-            <main className="flex-1">
-                {children}
-            </main>
+            <main className="flex-1">{children}</main>
             <Footer />
         </div>
-    )
+    );
 }
