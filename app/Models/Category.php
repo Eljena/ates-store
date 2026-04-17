4a8f7ecf219b3f6c19d701e2e85bee9f03c1bda6
Eljena <104090886+Eljena@users.noreply.github.com>
2026-04-17 15:29:12 +0200

    seed categories to Category table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
+    protected $fillable = [
+        'name',
+        'slug',
+    ];    
+
     public function products(): HasMany 
     {
         return $this->hasMany(Product::class);
