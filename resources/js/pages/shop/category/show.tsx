29d6e9ea3c94e610d9f4d1d4d968839aee2441ae
Eljena <104090886+Eljena@users.noreply.github.com>
2026-04-20 20:16:27 +0200

    add breadcumbs

--- resources/js/pages/shop/category/show.tsx
+++ resources/js/pages/shop/category/show.tsx
@@ -1,5 +1,6 @@
+import { Breadcrumbs } from '@/components/breadcrumbs';
 import Layout from '@/layouts/shop/layout';
-import type { Category, Product } from '@/types/shop';
+import type { Category } from '@/types/shop';
 
 type Props = {
     category: Category;
@@ -8,6 +9,14 @@ type Props = {
 export default function Show({ category }: Props) {
     return (
         <Layout>
+            <div className="pb-5">
+                <Breadcrumbs
+                    breadcrumbs={[
+                        { title: 'Startseite', href: '/' },
+                        { title: category.name, href: '/' },
+                    ]}
+                />
+            </div>
             <h1 className="text-2xl font-bold">{category.name}</h1>
         </Layout>
     );
