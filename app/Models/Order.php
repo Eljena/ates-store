cbfbc3d57a8f5f2d2bd1a9ec72f0fc00c70ed1fc
Eljena <104090886+Eljena@users.noreply.github.com>
2026-05-07 16:34:58 +0200

    feat: add Order and OrderItem models with migrations

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'customer_firstName',
+        'customer_lastName',
+        'customer_email',
+        'customer_address',
+        'status',
+    ];
+
+    public function orderItems(): HasMany {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+}
