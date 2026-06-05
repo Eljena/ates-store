7bb73aa0f1375049704ab7fd62804d3adaaeb2e3
Eljena <104090886+Eljena@users.noreply.github.com>
2026-06-05 11:28:39 +0200

    refactor: rename admin settings title

--- resources/js/pages/admin/settings.tsx
+++ resources/js/pages/admin/settings.tsx
@@ -3,7 +3,7 @@ import { Head } from '@inertiajs/react';
 export default function index() {
     return (
         <>
-            <Head title="Admin" />
+            <Head title="Admin Einstellungen" />
             <div className="mx-auto">
                 <p>Hallo aus dem Admin-Bereich</p>
             </div>
@@ -14,7 +14,7 @@ export default function index() {
 index.layout = {
     breadcrumbs: [
         {
-            title: 'Einstellungen',
+            title: 'Admin Einstellungen',
             href: '/admin/settings',
         },
     ],
