82a7f21fa2ec5a0b1f2c998c95548c0bd56250e8
Eljena <104090886+Eljena@users.noreply.github.com>
2026-07-16 23:10:24 +0200

    Fix order of optimize commands in deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,10 @@ git clean -fd --exclude=.env
 git pull origin main
 
 composer install --no-dev --optimize-autoloader
+php artisan optimize:clear
+
 npm ci
 npm run build
 
 php artisan migrate --force
-php artisan optimize:clear
 php artisan optimize
