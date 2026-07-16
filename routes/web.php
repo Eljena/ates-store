25b2784abe259708a4b53be2c6c6123e45df6c56
Eljena <104090886+Eljena@users.noreply.github.com>
2026-07-16 22:30:23 +0200

    fix inconsistency route declarations

--- routes/web.php
+++ routes/web.php
@@ -46,8 +46,8 @@ Route::middleware(['auth', 'verified', 'role:admin,product_manager'])->group(fun
     Route::get('/admin/products/create', [AdminProductController::class, 'create'])->name('admin.products.create');
     Route::post('/admin/products', [AdminProductController::class, 'store'])->name('admin.products.store');
 
-    Route::get('admin/products/{product}/edit', [AdminProductController::class, 'edit'])->name('admin.products.edit');
-    Route::put('admin/products/{product}', [AdminProductController::class, 'update'])->name('admin.products.update');
+    Route::get('/admin/products/{product}/edit', [AdminProductController::class, 'edit'])->name('admin.products.edit');
+    Route::put('/admin/products/{product}', [AdminProductController::class, 'update'])->name('admin.products.update');
 
     Route::delete('/admin/products/{product}', [AdminProductController::class, 'destroy'])->name('admin.products.destroy');
 });
